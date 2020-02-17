67583d42f1ebe102613b4249922f8a2065b69cad
rrajesh0205 <61044061+rrajesh0205@users.noreply.github.com>
2020-02-17 19:52:22 +0530

    This code works perfectly for one user. (rajesh) and gets him logged in

--- mySQLConnector_Usertest.py
+++ mySQLConnector_Usertest.py
@@ -5,23 +5,14 @@ db = mysql.connect(host = "localhost", user = "rrajesh0205", passwd = "Longines0
 ## defining the Query to fetch the user
 
 query = "SELECT name FROM usernew where id =%s"
-id = (1,)
+id=(1,)
 cursor = db.cursor()
 cursor.execute(query, id)
 record = cursor.fetchone()
 
-## defining the Query to fetch the password
-
-query = "SELECT code FROM usernew where id =%s"
-id = (1,)
-cursor = db.cursor()
-cursor.execute(query, id)
-record1 = cursor.fetchone()
-
 ## Assigning the fetched data to the User Input data
 
-user_name = record[0]
-uipw = record1[0]
+ui = str(record[0])
 
 ## Getting the user input and cross checking with the database
 
@@ -30,14 +21,17 @@ count = 1
 while count < 4:
     ui =    input("Enter your username : ")
     uipw =  input("Enter your password : ")
-    if ui == record[0] and uipw == record1[0]:
+    if record[0] == ui:
         print("Hi!", ui,", You have been logged in.")
         count = 5
     else:
         print("Hi!", ui,", The credentials entered are not matching in our records.")
         if count == 3:
             print("Hi!", ui,", You have exceeded the maximum number of attempts.")
+            print("Hi!", ui, ", Maybe you are a new user, want to create a new user")
+
         count += 1
+
 
 ## Closing the cursor and the database
 
